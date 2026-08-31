5ae0b0d1e11c63bb8d43367c246ccb1a21870249
DmytroParamonov <paramonov1488@gmail.com>
2026-08-31 17:57:52 +0300

    Register button-first UX router

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -1,5 +1,6 @@
 from .common import router as common_router
 from .deals import router as deals_router
 from .game import router as game_router
+from .ux import router as ux_router
 
-__all__ = ["common_router", "deals_router", "game_router"]
+__all__ = ["ux_router", "common_router", "deals_router", "game_router"]
