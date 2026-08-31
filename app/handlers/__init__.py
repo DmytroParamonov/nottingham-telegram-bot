a34ce7c50766cbeb5ebe459bdb93725840910d40
DmytroParamonov <paramonov1488@gmail.com>
2026-08-31 17:45:31 +0300

    Register deal negotiation router

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -1,4 +1,5 @@
 from .common import router as common_router
+from .deals import router as deals_router
 from .game import router as game_router
 
-__all__ = ["common_router", "game_router"]
+__all__ = ["common_router", "deals_router", "game_router"]
