b43c75ff7424c9b5640682b74f7362cef4f11698
DmytroParamonov <paramonov1488@gmail.com>
2026-09-01 13:25:10 +0300

    Register beginner rules router

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -1,6 +1,13 @@
 from .common import router as common_router
 from .deals import router as deals_router
 from .game import router as game_router
+from .rules_help import router as rules_help_router
 from .ux import router as ux_router
 
-__all__ = ["ux_router", "common_router", "deals_router", "game_router"]
+__all__ = [
+    "rules_help_router",
+    "ux_router",
+    "common_router",
+    "deals_router",
+    "game_router",
+]
